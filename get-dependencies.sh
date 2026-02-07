1060b401e4231838efe229bc9fb1b9fd34448443
Link4Electronics <link4electronics@gmail.com>
2026-02-07 06:23:18 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -13,6 +13,9 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package cannonball-git
+if [ "${DEVEL_RELEASE-}" = 1 ]; then
+  make-aur-package cannonball-git
+else
+  make-aur-package cannonball
 
 # If the application needs to be manually built that has to be done down here
