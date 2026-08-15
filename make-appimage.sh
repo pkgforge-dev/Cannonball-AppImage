e3b8884f9167d55a77491023119db8a6191df1cd
Link4Electronics <link4electronics@gmail.com>
2026-08-15 06:47:12 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,7 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q cannonball-git | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q cannonball-git | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
@@ -19,8 +19,6 @@ mkdir -p ./AppDir/bin
 mv -v /opt/cannonball/res ./AppDir/bin
 mv -v /opt/cannonball/config.xml ./AppDir/bin
 wget -O ./AppDir/bin/res/gamecontrollerdb.txt https://raw.githubusercontent.com/mdqinc/SDL_GameControllerDB/master/gamecontrollerdb.txt
-
-# Additional changes can be done in between here
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
