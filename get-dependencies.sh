9cb93fd263c68b5a3d7fa74bf791a19527362db1
Link4Electronics <link4electronics@gmail.com>
2026-08-19 07:52:02 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -34,5 +34,5 @@ mv -v ./res/config.xml ../AppDir/bin
 sed -i s/hires\>0/hires\>1/g ../AppDir/bin/config.xml
 mv -v ./res/tilemap.bin ./res/tilepatch.bin  ../AppDir/bin/res
 cd ../
-git clone https://github.com/jacktang/outrun-roms
-mv -v ./outrun-roms/* ./AppDir/bin/roms
+#git clone https://github.com/jacktang/outrun-roms
+#mv -v ./outrun-roms/* ./AppDir/bin/roms
