8c9e87977709c469345c8214d752638526023634
Link4Electronics <link4electronics@gmail.com>
2026-08-19 07:04:58 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -14,9 +14,6 @@ export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/cannonball
-
-mv -v /opt/cannonball/res ./AppDir/bin
-mv -v /opt/cannonball/config.xml ./AppDir/bin
 wget -O ./AppDir/bin/res/gamecontrollerdb.txt https://raw.githubusercontent.com/mdqinc/SDL_GameControllerDB/master/gamecontrollerdb.txt
 
 # Turn AppDir into AppImage
