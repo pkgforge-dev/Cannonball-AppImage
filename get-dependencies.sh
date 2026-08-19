a5aa4a34fbe1f35a7ee7b9e4cbf103dde44c0e7d
Link4Electronics <link4electronics@gmail.com>
2026-08-19 07:58:36 -0300

    compile manually (#15)
    * Update get-dependencies.sh
    
    * Update get-dependencies.sh
    
    * Update get-dependencies.sh
    
    * Update get-dependencies.sh
    
    * Update get-dependencies.sh
    
    * Update make-appimage.sh
    
    * Update make-appimage.sh
    
    * Update make-appimage.sh
    
    * Update get-dependencies.sh
    
    * Update get-dependencies.sh
    
    * Update get-dependencies.sh
    
    * Update get-dependencies.sh
    
    * Create cannonball.desktop
    
    * Update cannonball.desktop
    
    * Create no-force-alsa.patch
    
    * Update get-dependencies.sh
    
    * Update make-appimage.sh
    
    * Update get-dependencies.sh
    
    * Update get-dependencies.sh
    
    * Update get-dependencies.sh
    
    * Update get-dependencies.sh
    
    * Update get-dependencies.sh
    
    * bump `LATEST_VERSION` [skip ci]
    
    * Update get-dependencies.sh
    
    * Update get-dependencies.sh
    
    * bump `LATEST_VERSION` [skip ci]
    
    * Update get-dependencies.sh
    
    * Update get-dependencies.sh
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,30 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm libdecor
+pacman -Syu --noconfirm \
+    boost       \
+    cmake       \
+    libdecor    \
+    sdl2-compat
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-make-aur-package cannonball-git
+echo "Building Cannonball..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/djyt/cannonball"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone "$REPO" ./cannonball
+echo "$VERSION" > ~/version
 
-# If the application needs to be manually built that has to be done down here
+mkdir -p ./AppDir/bin/res
+mkdir -p ./AppDir/bin/roms
+cd ./cannonball
+patch -p1 -N -r - -i ../no-force-alsa.patch
+cmake -S ./cmake -B build -D CMAKE_BUILD_TYPE=Release -DTARGET=linux.cmake -DOpenGL_GL_PREFERENCE=GLVND -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+cmake --build build -j$(nproc)
+mv -v build/cannonball ../AppDir/bin
+mv -v ./res/config.xml ../AppDir/bin
+sed -i s/hires\>0/hires\>1/g ../AppDir/bin/config.xml
+mv -v ./res/tilemap.bin ./res/tilepatch.bin  ../AppDir/bin/res
--- make-appimage.sh
+++ make-appimage.sh
@@ -3,21 +3,16 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q cannonball-git | awk '{print $2; exit}')
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/256x256/apps/cannonball.png
-export DESKTOP=/usr/share/applications/cannonball.desktop
+export ICON=https://raw.githubusercontent.com/djyt/cannonball/refs/heads/master/res/icon.png
 export STARTUPWMCLASS=cannonball
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /opt/cannonball
-mkdir -p ./AppDir/bin
-mv -v /opt/cannonball/res ./AppDir/bin
-mv -v /opt/cannonball/config.xml ./AppDir/bin
+quick-sharun ./AppDir/bin/cannonball
 wget -O ./AppDir/bin/res/gamecontrollerdb.txt https://raw.githubusercontent.com/mdqinc/SDL_GameControllerDB/master/gamecontrollerdb.txt
 
 # Turn AppDir into AppImage
