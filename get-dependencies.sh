0f76d611c1c3a3028b0eb166a2e3f7852bc1f449
Link4Electronics <link4electronics@gmail.com>
2026-08-19 06:54:57 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -12,7 +12,12 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-make-aur-package cannonball-git
+echo "Building Cannonball..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/djyt/cannonball"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone "$REPO" ./cannonball
+echo "$VERSION" > ~/version
 
-# If the application needs to be manually built that has to be done down here
+mkdir -p ./AppDir/bin
+
