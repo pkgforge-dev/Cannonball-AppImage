b5022be9bfe8f92a9177ca345fe13f2b38d69e6e
Link4Electronics <link4electronics@gmail.com>
2026-08-30 13:22:04 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,7 +9,7 @@ export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/djyt/cannonball/refs/heads/master/res/icon.png
 export STARTUPWMCLASS=cannonball
-export USE_HOST_DRIVERS_EXPERIMENTAL=1
+export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/cannonball
