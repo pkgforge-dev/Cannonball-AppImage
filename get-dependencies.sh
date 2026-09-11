1e8ba4ace9691a61f7cf2738467c793e2b2e36ec
Link4Electronics <link4electronics@gmail.com>
2026-09-10 21:31:24 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -28,7 +28,6 @@ cd ./cannonball
 patch -p1 -N -r - -i ../no-force-alsa.patch
 cmake -S ./cmake -B build -DCMAKE_BUILD_TYPE=Release -DTARGET=linux.cmake -DOpenGL_GL_PREFERENCE=GLVND -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 cmake --build build -j$(nproc)
-mv -v build/cannonball ../AppDir/bin
-mv -v ./res/config.xml ../AppDir/bin
+mv -v ./build/cannonball ./res/config.xml ../AppDir/bin
 sed -i s/hires\>0/hires\>1/g ../AppDir/bin/config.xml
 mv -v ./res/tilemap.bin ./res/tilepatch.bin  ../AppDir/bin/res
